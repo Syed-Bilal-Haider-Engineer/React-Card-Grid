ee97d70ea851a7018d65384b9018f7fdc96a0c56
syed bilal haider <bilaldev151214@gmail.com>
2026-05-15 14:27:33 +0200

    update: change end point of image fetching

--- frontend/my-app/src/services/image.api.ts
+++ frontend/my-app/src/services/image.api.ts
@@ -35,3 +35,19 @@ export const uploadImage = async ({
     throw error;
   }
 };
+
+export const fetchGalleryApi = async (email: string) => {
+  const response = await fetch(
+    `${import.meta.env.VITE_API_URL}/gallery/${email}`
+  );
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(
+      data.error || "Failed to fetch gallery"
+    );
+  }
+
+  return data;
+};
