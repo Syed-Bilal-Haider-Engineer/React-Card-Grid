a2f6323d2a15b738b90692710a50d7641d03d936
syed bilal haider <bilaldev151214@gmail.com>
2026-05-19 15:06:26 +0200

    update: add Lambda function to fetch all images

--- serverless/src/handlers/gallery/getGallery.js
+++ serverless/src/handlers/gallery/getGallery.js
@@ -2,6 +2,7 @@ const sendResponse = require("../../utils/response");
 
 const {
   getUserGallery,
+  getUserAllGalleryImages
 } = require("../../services/galleryService.js");
 
 exports.handler = async (event) => {
@@ -9,6 +10,19 @@ exports.handler = async (event) => {
     const { email } = event.pathParameters;
 
     const gallery = await getUserGallery(email);
+
+    return sendResponse(200, gallery);
+  } catch (error) {
+    return sendResponse(500, {
+      error: error.message,
+    });
+  }
+}
+
+exports.handler = async () => {
+  try {
+
+    const gallery = await getUserAllGalleryImages();
 
     return sendResponse(200, gallery);
   } catch (error) {
